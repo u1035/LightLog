d16530cb88188170d83d0e099858cd82296a86b0
Александр Мохов <u1035@mail.ru>
2020-05-23 22:13:17 +0300

    Added log file writing functionality

--- LightLog/LightLog.cs
+++ LightLog/LightLog.cs
@@ -1,63 +1,117 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace LightLog
 {
-    public class LightLog
+    public static class Logger
     {
-        public static string LogFileName { get; private set; }
+        private static bool _logFileSet;
+
+        private static string _logFileName;
+        public static string LogFileName
+        {
+            get => _logFileName;
+            set
+            {
+                _logFileName = value;
+                _logFileSet = FileNameCorrect(value);
+            }
+        }
+
+        [UsedImplicitly]
         public static ObservableCollection<LogRecord> LogRecords { get; } = new ObservableCollection<LogRecord>();
 
-        private static StreamWriter _logFile;
-
-        public static void InitFileLog(string logFileName = "")
-        {
-            if (string.IsNullOrWhiteSpace(logFileName)) return;
-            CreateOrOpenLogFile(logFileName);
-
-            LogFileName = logFileName;
-        }
 
         public static void ClearLog()
         {
             LogRecords.Clear();
         }
 
-        private static void CreateOrOpenLogFile(string logFileName)
+        public static void Error(string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(DateTime.Now, sender, message, LogLevel.Error));
+        }
+
+        public static void Warning(string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(DateTime.Now, sender, message, LogLevel.Warning));
+        }
+
+        public static void Notice(string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(DateTime.Now, sender, message, LogLevel.Notice));
+        }
+
+        public static void Info(string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(DateTime.Now, sender, message, LogLevel.Info));
+        }
+
+        public static void Debug(string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(DateTime.Now, sender, message, LogLevel.Debug));
+        }
+
+        public static void Trace(string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(DateTime.Now, sender, message, LogLevel.Trace));
+        }
+
+        public static void AddRecord(LogLevel level, string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(DateTime.Now, sender, message, level));
+        }
+
+        public static void AddRecord(DateTime time, LogLevel level, string message, [CallerMemberName] string sender = "")
+        {
+            AddRecord(new LogRecord(time, sender, message, level));
+        }
+
+        private static void AddRecord(LogRecord record)
+        {
+            if (_logFileSet) AppendRecordToFile(record);
+
+            LogRecords.Add(record);
+        }
+
+        private static void AppendRecordToFile(LogRecord record)
+        {
+            var formattedRecord = $"{record.Time}|{record.Sender}|{record.Level}|{record.Message}";
+            try
+            {
+                File.AppendAllText(LogFileName, $"{formattedRecord}" + Environment.NewLine);
+            }
+            catch (Exception)
+            { /*ignored*/ }
+        }
+
+
+
+        private static bool FileNameCorrect(string filename)
         {
             try
             {
-                _logFile = new StreamWriter(logFileName, true);
+                if (filename.IndexOf(Path.DirectorySeparatorChar) != -1)
+                {
+                    //path contains "\" (for Windows) - so assume it's absolute path, let's try to create directory
+                    Directory.CreateDirectory(Path.GetFullPath(filename));
+                }
+
+                File.AppendAllText(filename, GetLogInitMessage());
+                return true;
             }
             catch (Exception)
             {
-                //ignored
+                return false;
             }
         }
 
-
-        private static void AppendRecordToFile()
+        private static string GetLogInitMessage()
         {
-
+            return $"{DateTime.Now:O} - Log file initialized" + Environment.NewLine;
         }
-    }
-
-
-    public sealed class Logger
-    {
-        private static readonly LightLog _instance = new LightLog();
-
-        // Explicit static constructor to tell C# compiler
-        // not to mark type as beforefieldinit
-        static Logger()
-        {
-        }
-
-        private Logger()
-        {
-        }
-
-        public static LightLog Instance => _instance;
     }
 }
---
+++ LogTestApp_Net472/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LightLog;
+
+namespace LogTestApp_Net472
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Logger.LogFileName = "test.log";
+            Logger.Debug("Program started");
+        }
+    }
+}
